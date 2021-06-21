76d81717a8bff4542172a0360a09a73560862eba
mark.bukovich46@gmail.com <mark.bukovich46@gmail.com>
2021-06-20 23:05:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/chessmark/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.chessmark;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Add Switch statement to perform an appropriate action when a certain item in the menu is clicked
+        // This switch statement will compare item.itemId to the ids of the menu items
+        return true;
+    }
+}
